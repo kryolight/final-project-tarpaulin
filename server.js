cf55056c5dc27a35066895d55c267677ab7b87a2
Martin Nguyen <martinnguyen0523@gmail.com>
2020-06-09 00:27:33 -0700

    implemented rate limiting

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const {
     connectToDB
 } = require('./lib/mongo');
 
+const { applyRateLimit } = require('./lib/redis');
+
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -13,6 +15,8 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use(applyRateLimit);
 
 app.use('/', (req, res) => {
     res.status(200).send({
